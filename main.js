fbd8115b4e7acfe9aa1e095c15b8b8134819855a
SaraCremonesi <saracrem00@gmail.com>
2020-06-26 17:36:59 +0200

    commenti

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
+  // Eseguo la chiamata ajax
 	$.ajax(
       {
         url: 'https://flynn.boolean.careers/exercises/api/array/music',
         method: 'GET',
         success: function(randomCD) {
           var cdResponse = randomCD.response;
+          // Per non aggiungere eccessiva logica al success,
+          // creo una funzione esterna da richiamare poi qui (dandogli "in pasto" il response)
           printCD(cdResponse);
         },
         error: function(richiesta, stato, errore) {
@@ -13,14 +16,19 @@ $(document).ready(function() {
       }
     );
   });
-  function printCD(arrayCD) {
 
+  // Creo la funzione per stampare a schermo i dati dei vari cd
+  function printCD(arrayCD) {
+    // Handlebars
     var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
 
+    // Creo un ciclo for per stamoare a schermo tutti i cd
     for (i = 0; i < arrayCD.length; i++) {
       var cd = arrayCD[i]
+      // Handlebars
       var html = template(cd);
+      // Aggiungo ogni cd nell'html
       $('.cds-container').append(html);
     }
   };
